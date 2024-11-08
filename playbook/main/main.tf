8df5af9638eaf22fa36f8541c17c7b7e0ffb9cd5
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-11-08 15:44:11 -0300

    chore: Cleanup tf

---
+++ playbook/main/main.tf
@@ -0,0 +1,77 @@
+locals {
+  namespace          = "hydra-doom"
+  operator_image     = "ghcr.io/demeter-run/doom-patrol-operator:sha-ded72a2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "hydra-doom-tf"
+    key    = "clusters/hydra-doom-dev-cluster/tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.22.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "hydra-doom-dev-cluster"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "hydra-doom-dev-cluster"
+  }
+}
+
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = local.namespace
+  }
+}
+
+variable "blockfrost_key" {
+  type = string
+}
+
+variable "dmtr_project_id" {
+  type = string
+}
+
+variable "dmtr_api_key" {
+  type = string
+}
+
+variable "dmtr_port_name" {
+  type = string
+}
+
+module "stage1" {
+  source = "../../bootstrap/stage1/"
+}
+
+module "stage2" {
+  source     = "../../bootstrap/stage2"
+  depends_on = [module.stage1]
+
+  namespace                  = local.namespace
+  external_domain            = "us-east-1.hydra-doom.sundae.fi"
+  operator_image             = local.operator_image
+  hydra_node_image           = "ghcr.io/cardano-scaling/hydra-node:latest"
+  sidecar_image              = "ghcr.io/demeter-run/doom-patrol-hydra:477d1ca9081693999580b2b7827df2fc87bb4cb8"
+  open_head_image            = "ghcr.io/demeter-run/doom-patrol-hydra:477d1ca9081693999580b2b7827df2fc87bb4cb8"
+  control_plane_image        = "ghcr.io/demeter-run/doom-patrol-hydra:477d1ca9081693999580b2b7827df2fc87bb4cb8"
+  blockfrost_key             = var.blockfrost_key
+  external_port              = 80
+  admin_key_path             = "${path.module}/admin.sk"
+  admin_addr                 = "addr_test1vpgcjapuwl7gfnzhzg6svtj0ph3gxu8kyuadudmf0kzsksqrfugfc"
+  dmtr_project_id            = var.dmtr_project_id
+  dmtr_api_key               = var.dmtr_api_key
+  dmtr_port_name             = var.dmtr_port_name
+  hydra_scripts_tx_id        = "03f8deb122fbbd98af8eb58ef56feda37728ec957d39586b78198a0cf624412a"
+}
